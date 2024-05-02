c39504203c2622a140ae3433087df553bb646b99
ANA CAROLINA LAMBERTUCCI SIMAS <ana.simas@ielusc.br>
2024-05-02 13:52:23 -0300

    change: alterações visuais na home

--- lib/src/modules/home/page/home_page.dart
+++ lib/src/modules/home/page/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:fast_location/src/shared/colors/app_colors.dart';
 import 'package:flutter/material.dart';
 
@@ -26,9 +28,31 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: AppColors.appPageBackground,
-      body: Text('Home Page'),
-    );
+    return Scaffold(
+        body: Center(
+            child: Column(children: [
+          Container(
+              color: AppColors.appBarContainer,
+              height: 250,
+              width: 300,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(size: 100, color: Colors.green, Icons.directions),
+                  Text("Faça uma busca",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold))
+                ],
+              )),
+          ElevatedButton(
+              onPressed: null,
+              style: ElevatedButton.styleFrom(primary: Colors.green),
+              child: Text("Localizar endereço",
+                  style: TextStyle(color: Colors.white, fontSize: 10)))
+        ])),
+        backgroundColor: AppColors.appPageBackground);
   }
 }
